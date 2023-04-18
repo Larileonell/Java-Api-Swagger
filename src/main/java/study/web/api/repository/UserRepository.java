b21b247c1947031e7261a9205e0416dc4db16f3b
Allana Larissa <larissaleonel98@gmail.com>
2023-04-18 08:24:38 -0300

    add findbyid and deletebyid

--- src/main/java/study/web/api/repository/UserRepository.java
+++ src/main/java/study/web/api/repository/UserRepository.java
@@ -1,7 +1,34 @@
 package study.web.api.repository;
 
 import org.springframework.stereotype.Repository;
+import study.web.api.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class UserRepository {
+    public void save (Usuario usuario){
+        if (usuario.getId() == null)
+            System.out.println("SAVE- Recebendo o usuário na camada de repositorio");
+        else
+            System.out.println("UPDATE - Recebendo o usuário na camada de repositorio");
+        System.out.println(usuario);
+    }
+    public void deleteById  (Integer id){
+        System.out.printf(String.format("DELETE/ID - Recebendo o id para excluir um usuário%n", id));
+        System.out.println(id);
+    }
+    public List<Usuario> ListAll(){
+        System.out.println("Listando usuários do sistema");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("larissa", "harrystyles"));
+        usuarios.add(new Usuario("joanderson","lovelly"));
+        return usuarios;
+    }
+    public Usuario finById(Integer id){
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("gleyson","password");
+    }
+
 }
