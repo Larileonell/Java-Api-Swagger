1040078cdb46835f2ed1f0022e5487c08f0fc1fb
Allana Larissa <larissaleonel98@gmail.com>
2023-04-18 08:08:33 -0300

    Initial commit

---
+++ src/main/java/study/web/api/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package study.web.api.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+}
---
+++ src/test/java/study/web/api/ApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
